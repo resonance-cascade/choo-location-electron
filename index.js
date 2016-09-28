e4b183f34b64f729eea4d2dfe2768096fb6d1c7b
sethvincent <sethvincent@gmail.com>
2016-09-27 17:02:03 -0700

    add openExternal option

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-module.exports = {
-  namespace: 'location',
-  state: {
-    pathname: '/'
-  },
-  reducers: {
-    pathname: function (data, state) {
-      return { pathname: data.pathname }
-    }
-  },
-  subscriptions: [
-    function catchLinks (send, done) {
-      window.onclick = function (e) {
-        var node = (function traverse (node) {
-          if (!node) return
-          if (node.localName !== 'a') return traverse(node.parentNode)
-          if (node.href === undefined) return traverse(node.parentNode)
-          if (window.location.host !== node.host) return traverse(node.parentNode)
-          return node
-        })(e.target)
+var shell = require('electron').shell
 
-        if (!node) return
-        e.preventDefault()
-        var href = node.href.replace('file://', '')
+module.exports = function (options) {
+  options = options || {}
+  var openExternal = options.openExternal || false
 
-        send('location:pathname', { pathname: href.replace(/#$/, '') }, done)
+  return {
+    namespace: 'location',
+    state: {
+      pathname: '/'
+    },
+    reducers: {
+      pathname: function (data, state) {
+        return { pathname: data.pathname }
       }
-    }
-  ]
+    },
+    subscriptions: [
+      function catchLinks (send, done) {
+        window.onclick = function (e) {
+          var node = (function traverse (node) {
+            if (!node) return
+            if (node.localName !== 'a') return traverse(node.parentNode)
+            if (node.href === undefined) return traverse(node.parentNode)
+            return node
+          })(e.target)
+
+          if (!node || !node.href) return
+          e.preventDefault()
+
+          if (openExternal && node.href.indexOf('http') > -1) {
+            return shell.openExternal(node.href)
+          }
+
+          var href = node.href.replace('file://', '')
+          send('location:pathname', { pathname: href.replace(/#$/, '') }, done)
+        }
+      }
+    ]
+  }
 }
